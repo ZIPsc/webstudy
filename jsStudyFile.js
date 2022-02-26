bc9273f25551e2a609fcbf50d32ed7431c854040
ZIPsc <didgmdcjf2544@gmail.com>
2022-02-27 00:39:56 +0900

    study2

--- jsStudyFile.js
+++ jsStudyFile.js
@@ -1,10 +1,30 @@
-function doSomething(add){
-    console.log(add);
+// function add(num1, num2){
+//     return num1 + num2;
+// }
 
-}
-function add(a, b){
-    const sum = a + b;
-    return sum;
-}
+// function print(a, b){
+//     console.log(`a : ${a}, b : ${b}`);
+//     console.log(`test`);
+// }
+// print(4, 5);
 
-doSomething(add);
+// const doSomething = add;
+
+// console.log(doSomething(1, 2));
+// console.log(add(3,4));
+
+// function divide(num1, num2){
+//     return num1 / num2;
+// }
+
+// function surprise(operator){
+//     const result = operator(10, 30);
+//     console.log(result);
+// }
+// surprise(add);
+// surprise(divide);
+const ellie = {age : 21, name : "ele"};
+console.log(ellie.age);
+console.log(ellie.name);
+ellie.age = 25;
+console.log(ellie.age);
